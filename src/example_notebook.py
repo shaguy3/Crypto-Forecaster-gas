9303f7893687a09f1b74b4a680a6bc0788996281
guy.shamilyan <guy.shamilyan@ibm.com>
2021-12-04 10:27:24 +0200

    Updated the example_notebook

--- src/example_notebook.py
+++ src/example_notebook.py
@@ -1,125 +1,199 @@
-import datetime
-import os,sys
-import numpy as np
 import pandas as pd
-import gresearch_crypto
+import numpy as np
+from datetime import datetime
+from os import path
+import plotly.graph_objects as go
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import time
+import scipy.stats as stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.multioutput import MultiOutputRegressor
 
-env = gresearch_crypto.make_env()
+def main():
+    data_folder = "/home/ec2-user/Crypto-Forecaster-gas/data/"
+    crypto_df = pd.read_csv(data_folder + 'train.csv')
+    asset_details = pd.read_csv(data_folder + 'asset_details.csv')
 
-class gresearch_guada():
-    """docstring for gresearch_guada"""
-    def __init__(self):
-        #super(gresearch_guada, self).__init__()
-        ### 训练集
-        self.train = '/home/ec2-user/Crypto-Forecaster-gas/data/train.csv'
-        ### 补充训练数据集——（验证集）
-        self.supplemental_train = '/home/ec2-user/Crypto-Forecaster-gas/data/supplemental_train.csv'
-        ### 资产信息数据集，包含14个虚拟货币资产
-        self.asset_details = '/home/ec2-user/Crypto-Forecaster-gas/data/asset_details.csv'
-        ### 测试数据样例
-        self.example_test = '/home/ec2-user/Crypto-Forecaster-gas/data/example_test.csv'
-        self.iter_test = env.iter_test()
+    btc = crypto_df[crypto_df["Asset_ID"]==1].set_index("timestamp") # Asset_ID = 1 for Bitcoin
+    btc_mini = btc.iloc[-200:] # Select recent data rows
 
-    def dataReader(self, datasetName):
-        ### 数据集读取
-        if datasetName == 'train':
-            ### 获取train数据集
-            df = pd.read_csv(self.train, usecols=['Target', 'Asset_ID', 'timestamp'], dtype={'Asset_ID': 'int8'})
-        elif datasetName == 'supplemental_train':
-            ### 获取supplemental_train
-            df = pd.read_csv(self.supplemental_train, usecols=['Target', 'Asset_ID', 'timestamp'], dtype={'Asset_ID': 'int8'})
-        else:
-            print("ERROR [1018] - message: 数据集传入参数错误!")
-        return df
+    fig = go.Figure(data=[go.Candlestick(x=btc_mini.index, open=btc_mini['Open'], high=btc_mini['High'], low=btc_mini['Low'], close=btc_mini['Close'])])
+    # fig.show()
 
-    def dataFillNan(self, train_data, columns, fillType):
-        ## 空缺值填充
-        if fillType == "0":
-            ### fixed value fillnan
-            fixed_value = input("请输入" + columns + "列固定填充值：(eg: 推荐值：" + str(Counter(train_data[columns]).most_common(3)) + ")：")
-            #self.logging.info("kaggle." + sys._getframe().f_code.co_name + ".service Message: 已获取缺失值填充参数：{'缺失值填充方法':'固定值填充','固定值':" + fixed_value + "},准备开始缺失值填充......")
-            train_data[columns].fillna(fixed_value, inplace=True)
-            if train_data[columns].isnull().any():
-                print("仍旧有空值")
-                #self.logging.error("kaggle." + sys._getframe().f_code.co_name + ".service Message: 数据集列" + columns +"缺失值以固定值方式填充失败，请查看原因！")
-            else:
-                print("该列无空值")
-                #self.logging.info("kaggle." + sys._getframe().f_code.co_name + ".service Message: 数据集列" + columns +"缺失值以固定值方式填充成功！")
-            return train_data
-        elif fillType == "1":
-            ### before value fillnan
-            return "前值填充法 暂未开放......"
-        elif fillType == "2":
-            ### 中位数填补
-            if train_data[columns].dtypes == 'float64' :
-                train_data[columns].fillna(train_data[columns].median(),inplace=True)
-            else:
-                print("该列数据类型不支持中位数填充！")
-            return train_data
-        elif fillType == "3":
-            ## 众数填补法
-            if train_data[columns].dtypes == 'float64' :
-                train_data[columns].fillna(train_data[columns].mode(),inplace=True)
-            else:
-                print("该列数据类型不支持众数填充！")
-                #continue
-            return train_data
-            #train_data[columns].fillna(train_data[columns].mode(),inplace=True)
-        elif fillType == "4":
-            ## 回归填补法
-            #self.logging.info("kaggle." + sys._getframe().f_code.co_name + ".service Message: 已获取缺失值填充参数：{'缺失值填充方法':'回归填充','当前列回归':" + str(train_data[columns].mode()) + "},准备开始缺失值填充......")
-            if train_data[columns].dtypes == 'float64':
-                #self.logging.info("kaggle." + sys._getframe().f_code.co_name + ".service Message: 已获取缺失值填充参数：{'缺失值填充方法':'回归填补法','填充值':}")
-                imp = IterativeImputer(max_iter=10, random_state=0)
-                imp.fit(train_data)
-                np.round(imp.transform(train_data))
-            else:
-                print("数据类型不支持回归填充法！")
-                #self.logging.info("kaggle." + sys._getframe().f_code.co_name + ".service Message: 数据类型不支持回归填充法！（说明*：该方法仅适用于缺失值为定量的数据类型）")
-        else:
-            ### 其他填充法
-            return "其他填充法 暂未开放......"
+    eth = crypto_df[crypto_df["Asset_ID"]==6].set_index("timestamp") # Asset_ID = 6 for Ethereum
+    print(eth.info(show_counts =True))
 
-    def datetimeProc(self, datasetName):
-        ## 数据集时间处理
-        datasetName['datetime'] = pd.to_datetime(datasetName['timestamp'], unit='s')
-        datasetName = datasetName.set_index('datetime').drop('timestamp', axis=1)
-        datasetName = datasetName[(datasetName.index.year == 2021) & (datasetName.index.month > 5)]
-        
-        dfs = {asset_id:datasetName[datasetName['Asset_ID'] == asset_id].resample('1min').interpolate().copy() for asset_id in datasetName['Asset_ID'].unique()}
-        ## delete $datasetName dataset
-        del datasetName
-        
-        for datasetName_test, datasetName_pred in self.iter_test:
-            datasetName_test['datetime'] = pd.to_datetime(datasetName_test['timestamp'], unit='s')
-            for _, row in datasetName_test.iterrows():
-                try:
-                    datasetName = dfs[row['Asset_ID']]
-                    closest_train_sample = datasetName.iloc[datasetName.index.get_loc(row['datasetName'], method='nearest')]
-                    datasetName_pred.loc[datasetName_pred['row_id'] == row['row_id'], 'Target'] = closest_train_sample['Target']
-                except:
-                    datasetName_pred.loc[datasetName_pred['row_id'] == row['row_id'], 'Target'] = 0
-            #gresearch_guada.
-            datasetName_pred_filled = gresearch_guada.dataFillNan(datasetName_pred, 'Target', '2')
-            return datasetName_pred_filled
-        #return datasetName
+    print(eth.isna().sum())
 
-## 类实例化
-gresearch_guada = gresearch_guada()
+    print(btc.head())
 
-## 读取train.csv
-df = gresearch_guada.dataReader("train")
+    beg_btc = btc.index[0].astype('datetime64[s]')
+    end_btc = btc.index[-1].astype('datetime64[s]')
+    beg_eth = eth.index[0].astype('datetime64[s]')
+    end_eth = eth.index[-1].astype('datetime64[s]')
 
-## 初步处理datetime字段
-df = gresearch_guada.datetimeProc(df)
+    print('BTC data goes from ', beg_btc, 'to ', end_btc)
+    print('Ethereum data goes from ', beg_eth, 'to ', end_eth)
 
-print(df)
+    print((eth.index[1:]-eth.index[:-1]).value_counts().head())
 
-## 数据集空白值填充
-#df_filled = gresearch_guada.dataFillNan(df, 'Target', '2')
+    eth = eth.reindex(range(eth.index[0],eth.index[-1]+60,60),method='pad')
+    print((eth.index[1:]-eth.index[:-1]).value_counts().head())
 
-#df_filled_pred = gresearch_guada.datetimeProc(df_filled)
+    # plot vwap time series for both chosen assets
+    f = plt.figure(figsize=(15,4))
 
+    # fill missing values for BTC
+    btc = btc.reindex(range(btc.index[0],btc.index[-1]+60,60),method='pad')
 
-env.predict(df)
-#print(df_filled_pred)
+    ax = f.add_subplot(121)
+    plt.plot(btc['Close'], label='BTC')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Bitcoin')
+
+    ax2 = f.add_subplot(122)
+    ax2.plot(eth['Close'], color='red', label='ETH')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Ethereum')
+
+    plt.tight_layout()
+    plt.show()
+
+    # auxiliary function, from datetime to timestamp
+    totimestamp = lambda s: np.int32(time.mktime(datetime.strptime(s, "%d/%m/%Y").timetuple()))
+
+    # create intervals
+    btc_mini_2021 = btc.loc[totimestamp('01/06/2021'):totimestamp('01/07/2021')]
+    eth_mini_2021 = eth.loc[totimestamp('01/06/2021'):totimestamp('01/07/2021')]
+
+    # plot time series for both chosen assets
+    f = plt.figure(figsize=(7,8))
+
+    ax = f.add_subplot(211)
+    plt.plot(btc_mini_2021['Close'], label='btc')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Bitcoin Close')
+
+    ax2 = f.add_subplot(212)
+    ax2.plot(eth_mini_2021['Close'], color='red', label='eth')
+    plt.legend()
+    plt.xlabel('Time')
+    plt.ylabel('Ethereum Close')
+
+    plt.tight_layout()
+    plt.show()
+
+    # define function to compute log returns
+    def log_return(series, periods=1):
+        return np.log(series).diff(periods=periods)
+
+    lret_btc = log_return(btc_mini_2021.Close)[1:]
+    lret_eth = log_return(eth_mini_2021.Close)[1:]
+    lret_btc.rename('lret_btc', inplace=True)
+    lret_eth.rename('lret_eth', inplace=True)
+
+    plt.figure(figsize=(8,4))
+    plt.plot(lret_btc)
+    plt.plot(lret_eth)
+    plt.show()
+
+    # join two asset in single DataFrame
+
+    lret_btc_long = log_return(btc.Close)[1:]
+    lret_eth_long = log_return(eth.Close)[1:]
+    lret_btc_long.rename('lret_btc', inplace=True)
+    lret_eth_long.rename('lret_eth', inplace=True)
+    two_assets = pd.concat([lret_btc_long, lret_eth_long], axis=1)
+
+    # group consecutive rows and use .corr() for correlation between columns
+    corr_time = two_assets.groupby(two_assets.index//(10000*60)).corr().loc[:,"lret_btc"].loc[:,"lret_eth"]
+
+    corr_time.plot()
+    plt.xticks([])
+    plt.ylabel("Correlation")
+    plt.title("Correlation between BTC and ETH over time")
+
+    # create dataframe with returns for all assets
+    all_assets_2021 = pd.DataFrame([])
+    for asset_id, asset_name in zip(asset_details.Asset_ID, asset_details.Asset_Name):
+        asset = crypto_df[crypto_df["Asset_ID"]==asset_id].set_index("timestamp")
+        asset = asset.loc[totimestamp('01/01/2021'):totimestamp('01/05/2021')]
+        asset = asset.reindex(range(asset.index[0],asset.index[-1]+60,60),method='pad')
+        lret = log_return(asset.Close.fillna(0))[1:]
+        all_assets_2021 = all_assets_2021.join(lret, rsuffix=asset_name, how="outer")
+
+    plt.imshow(all_assets_2021.corr())
+    plt.yticks(asset_details.Asset_ID.values, asset_details.Asset_Name.values)
+    plt.xticks(asset_details.Asset_ID.values, asset_details.Asset_Name.values, rotation='vertical')
+    plt.colorbar()
+
+    # Select some input features from the trading data: 
+    # 5 min log return, abs(5 min log return), upper shadow, and lower shadow.
+    upper_shadow = lambda asset: asset.High - np.maximum(asset.Close,asset.Open)
+    lower_shadow = lambda asset: np.minimum(asset.Close,asset.Open)- asset.Low
+
+    X_btc = pd.concat([log_return(btc.VWAP,periods=5), log_return(btc.VWAP,periods=1).abs(), 
+                upper_shadow(btc), lower_shadow(btc)], axis=1)
+    y_btc = btc.Target
+
+    X_eth = pd.concat([log_return(eth.VWAP,periods=5), log_return(eth.VWAP,periods=1).abs(), 
+                upper_shadow(eth), lower_shadow(eth)], axis=1)
+    y_eth = eth.Target
+
+    # select training and test periods
+    train_window = [totimestamp("01/05/2021"), totimestamp("30/05/2021")]
+    test_window = [totimestamp("01/06/2021"), totimestamp("30/06/2021")]
+
+    # divide data into train and test, compute X and y
+    # we aim to build simple regression models using a window_size of 1
+    X_btc_train = X_btc.loc[train_window[0]:train_window[1]].fillna(0).to_numpy()  # filling NaN's with zeros
+    y_btc_train = y_btc.loc[train_window[0]:train_window[1]].fillna(0).to_numpy()  
+
+    X_btc_test = X_btc.loc[test_window[0]:test_window[1]].fillna(0).to_numpy() 
+    y_btc_test = y_btc.loc[test_window[0]:test_window[1]].fillna(0).to_numpy() 
+
+    X_eth_train = X_eth.loc[train_window[0]:train_window[1]].fillna(0).to_numpy()  
+    y_eth_train = y_eth.loc[train_window[0]:train_window[1]].fillna(0).to_numpy()  
+
+    X_eth_test = X_eth.loc[test_window[0]:test_window[1]].fillna(0).to_numpy() 
+    y_eth_test = y_eth.loc[test_window[0]:test_window[1]].fillna(0).to_numpy()
+
+    # simple preprocessing of the data 
+    scaler = StandardScaler()
+
+    X_btc_train_scaled = scaler.fit_transform(X_btc_train)
+    X_btc_test_scaled = scaler.transform(X_btc_test)
+
+    X_eth_train_scaled = scaler.fit_transform(X_eth_train)
+    X_eth_test_scaled = scaler.transform(X_eth_test)
+
+    # implement basic ML baseline (one per asset)
+    lr = LinearRegression()
+    lr.fit(X_btc_train_scaled,y_btc_train)
+    y_pred_lr_btc = lr.predict(X_btc_test_scaled)
+
+    lr.fit(X_eth_train_scaled,y_eth_train)
+    y_pred_lr_eth = lr.predict(X_eth_test_scaled)
+
+    # we concatenate X and y for both assets
+    X_both_train = np.concatenate((X_btc_train_scaled, X_eth_train_scaled), axis=1)
+    X_both_test = np.concatenate((X_btc_test_scaled, X_eth_test_scaled), axis=1)
+    y_both_train = np.column_stack((y_btc_train, y_eth_train))
+    y_both_test = np.column_stack((y_btc_test, y_eth_test))
+
+    # define the direct multioutput model and fit it
+    mlr = MultiOutputRegressor(LinearRegression())
+    lr.fit(X_both_train,y_both_train)
+    y_pred_lr_both = lr.predict(X_both_test)
+
+    print('Test score for LR baseline: BTC', f"{np.corrcoef(y_pred_lr_btc, y_btc_test)[0,1]:.2f}", 
+                                    ', ETH', f"{np.corrcoef(y_pred_lr_eth, y_eth_test)[0,1]:.2f}")
+    print('Test score for multiple output LR baseline: BTC', f"{np.corrcoef(y_pred_lr_both[:,0], y_btc_test)[0,1]:.2f}", 
+                                                    ', ETH', f"{np.corrcoef(y_pred_lr_both[:,1], y_eth_test)[0,1]:.2f}")
+
+if __name__ == "__main__":
+    main()
